d0d8fa47b2e6af624b02cdffd50fe9bc09aaf007
guillermo <guillermo@upango.es>
2018-08-11 01:48:52 +0200

    Navigator sample

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_samples/main.dart';
+import 'package:flutter_samples/navigator_sample.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
